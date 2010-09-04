002a2ca0b35f6dae92aa3ebb5eb6cf10e21fda36
zachleat <zachleatherman@gmail.com>
2010-09-04 16:54:14 -0500

    SPACES NOT TABS.

--- src-test/sampleTest.js
+++ src-test/sampleTest.js
@@ -1,9 +1,9 @@
 SampleTest = TestCase("SampleTest");
 
 SampleTest.prototype.testTrue = function(){
-	assertTrue('True is true.', true);
+    assertTrue('True is true.', true);
 };
 
 SampleTest.prototype.testFalse = function(){
-	assertFalse('False is false.', false);
+    assertFalse('False is false.', false);
 };
