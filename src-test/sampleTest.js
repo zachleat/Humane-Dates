4100a203909385e77b99cac4e76636d14cb5c346
zachleat <zachleatherman@gmail.com>
2010-09-04 17:16:08 -0500

    Merging fun.

--- src-test/sampleTest.js
+++
@@ -1,9 +0,0 @@
-SampleTest = TestCase("SampleTest");
-
-SampleTest.prototype.testTrue = function(){
-    assertTrue('True is true.', true);
-};
-
-SampleTest.prototype.testFalse = function(){
-    assertFalse('False is false.', false);
-};
