e007b63a1fed4f16c86004083fce731c9881cd98
zachleat <zachleatherman@gmail.com>
2010-09-04 16:45:02 -0500

    Bad eclipse! DRUNK HULK WANT SPACES NOT TABS.

--- src-test/humaneDatesPluginTest.js
+++ src-test/humaneDatesPluginTest.js
@@ -2,39 +2,39 @@ HumaneDatesPluginTest = TestCase("HumaneDatesPluginTest");
 
 HumaneDatesPluginTest.prototype.getUTCDate = function(d)
 {
-	function pad(num)
-	{
-		return num < 10 ? '0' + num : num;
-	}
+    function pad(num)
+    {
+        return num < 10 ? '0' + num : num;
+    }
 
-	return d.getUTCFullYear() + '/' +
-		pad(d.getUTCMonth() + 1) + '/' +
-		pad(d.getUTCDate()) + 'T' +
-		pad(d.getUTCHours()) + ':' + 
-		pad(d.getUTCMinutes()) + ':' +
-		pad(d.getUTCSeconds()) + 'Z';
+    return d.getUTCFullYear() + '/' +
+        pad(d.getUTCMonth() + 1) + '/' +
+        pad(d.getUTCDate()) + 'T' +
+        pad(d.getUTCHours()) + ':' + 
+        pad(d.getUTCMinutes()) + ':' +
+        pad(d.getUTCSeconds()) + 'Z';
 };
 
 HumaneDatesPluginTest.prototype.testNow = function()
 {
-	var d = new Date;
-	d.setTime(d.getTime());
+    var d = new Date;
+    d.setTime(d.getTime());
 
-	jQuery(document.body).append('<span title="' + this.getUTCDate(d) + '">test</span>');
-	jQuery('span').humaneDates();
+    jQuery(document.body).append('<span title="' + this.getUTCDate(d) + '">test</span>');
+    jQuery('span').humaneDates();
 
-	assertEquals('Just Now', jQuery('span').html());
+    assertEquals('Just Now', jQuery('span').html());
 };
 
 
 HumaneDatesPluginTest.prototype.testMinutes = function()
 {
-	var d = new Date;
-	d.setTime(d.getTime() + 120*1000);
+    var d = new Date;
+    d.setTime(d.getTime() + 120*1000);
 
-	jQuery(document.body).append('<span title="' + this.getUTCDate(d) + '">test</span>');
-	jQuery('span').humaneDates();
+    jQuery(document.body).append('<span title="' + this.getUTCDate(d) + '">test</span>');
+    jQuery('span').humaneDates();
 
-	assertEquals('2 Minutes', jQuery('span').html());
+    assertEquals('2 Minutes', jQuery('span').html());
 };
 
