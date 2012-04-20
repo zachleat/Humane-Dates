7228f468fe7330e2ecc3b224388e8c0d8473de71
Christopher Pearce <hello@chrispearce.me>
2012-04-20 21:59:26 +0100

    Change my contribution from re-write to refactor.

--- humane.js
+++ humane.js
@@ -2,7 +2,7 @@
  * Javascript Humane Dates
  * Copyright (c) 2008 Dean Landolt (deanlandolt.com)
  * Re-write by Zach Leatherman (zachleat.com)
- * Re-Re-Write by Chris Pearce (github.com/Chrisui)
+ * Refactor by Chris Pearce (github.com/Chrisui)
  *
  * Adopted from the John Resig's pretty.js
  * at http://ejohn.org/blog/javascript-pretty-date
